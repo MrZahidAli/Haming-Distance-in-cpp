6d1573217429c5d49fcd2fb738593c96c370482b
MrZahidAli <46484008+MrZahidAli@users.noreply.github.com>
2019-11-28 00:56:30 +0500

    Update Haming Distance.cpp

--- Haming Distance.cpp
+++ Haming Distance.cpp
@@ -3,23 +3,16 @@ using namespace std;
 
 int hamingDistance(string str1, string str2){
 	int i = 0;
-	int dif = 0;
 	int distance = 0;
-	while(i < str1.length() && i < str2.length()){
+	while(i < str1.length() || i < str2.length()){
 		if(str1[i] != str2[i]){
-			dif++;
+			distance++;
 		}
 		i++;
 	}
-	if(str1.length() > str2.length()){
-		distance = dif + (str1.length() - str2.length());
-	}else{
-		distance = dif + (str2.length() - str1.length());
-	}
-	
 	cout << "distance is: " << distance;
 	return distance;
 }
 int main(){
-	hamingDistance("zahid", "jhinhid");
+	hamingDistance("zahid", "rashd");
 }
